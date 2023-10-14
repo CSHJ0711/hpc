907cb4b62504465b02a7f5fccc822cc5f32839ce
CSHJ <99818847+CSHJ0711@users.noreply.github.com>
2023-10-15 02:58:11 +0900

    HW3(OpenMP_Parallelize)

--- area.c
+++ area.c
@@ -2,31 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 
-double f(double x) {
-  return 1.0 / (2.0 * x * x * x); // 1/2z^3
+long millisecond()
+{
+   struct timeval tv;
+   gettimeofday(&tv, NULL);
+   return(1000*tv.tv_sec + tv.tv_usec/1000) ;
+}
+
+double f(double x)
+{
+   double temp;
+   temp = x*x+2;
+   return x/(temp*temp*temp);
 }
 
 main(int argc, char *argv[])
 {
-  long N; //user argument input N
-  int i; //for loop
-  double a = 2.0, b = 6.0; //range of x 0.0 to 2.0
-  double sum = 0.0;
-  double dx, x;
+   long N, i;
+   double a, b, area, local_area, dx, x;
+   long start, elapsed;
+   int nt, tid;
+   
+   if (argc != 3) {
+      printf("argument error\n");
+      exit(1);
+   }
 
-  if (argc != 2) { //is there 1 arguments?
-    printf("argument error\n");
-    exit(1);
-  }
-  N = atol(argv[1]); //char to long
-  
-  dx = (b-a)/N;
+   N = atol(argv[1]);
 
-  x = a;
-  for (i = 0; i < N; i++){
-    sum += 0.5 * (f(x) + f(x + dx)) * dx;
-    x += dx; 
-  }
-  printf("result :: %lf\n",sum);
-  return 0;
+   // sequential calculation of area
+   start = millisecond();
+   a = 0.0;
+   b = 2.0; 
+   dx = (b-a)/(double)N;
+
+   area = 0.0;
+
+   x = a;
+   for (i=0; i<N; i++) {
+      area += 0.5*(f(x)+f(x+dx))*dx;
+      x += dx;
+   }
+
+   elapsed = millisecond() - start;
+
+   printf("%5.10lf\n", area);
+   printf("elapsed time: %ld milliseconds\n", elapsed);
+   printf("GFLOPS: %10.2f\n", (N*16.0/(elapsed/1000.0))/1000000000.0);
+
+   nt = atoi(argv[2]);
+
+   // parallel(openMP) calculation of area
+   start = millisecond();
+   omp_set_num_threads(nt);
+
+   area = 0.0;
+   #pragma omp parallel private(i,x,tid,local_area) shared(dx,nt,area)
+   {
+      tid = omp_get_thread_num();
+      x = ((b-a)/nt)*tid;
+      #pragma omp for
+      for (i = 0; i < N; i++) {
+         local_area += 0.5*(f(x)+f(x+dx))*dx;
+         x += dx; //x = dx * i
+      }
+      #pragma omp atomic 
+      area += local_area;
+   }
+   elapsed = millisecond() - start;
+
+   printf("\narea(openMP): %5.10f\n", area);
+   printf("elapsed time(openMP): %ld milliseconds\n", elapsed);
+   printf("GFLOPS(openMP): %5.2f\n", (N*12.0/(elapsed/1000.0))/1000000000.0);
+
+   exit(0);
 }
